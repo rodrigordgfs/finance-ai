ad9a89fe46b256a923ff70f5148128b79b5e942e
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-12-23 06:19:25 -0300

    feat: add mulish font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { dark } from "@clerk/themes";
+import { Mulish } from "next/font/google";
+
+const mulish = Mulish({
+  subsets: ["latin-ext"],
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +19,9 @@ export default function RootLayout({
           baseTheme: dark,
         }}
       >
-        <body className="dark antialiased">{children}</body>
+        <body className={`${mulish.className} dark antialiased`}>
+          {children}
+        </body>
       </ClerkProvider>
     </html>
   );
